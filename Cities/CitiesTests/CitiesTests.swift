9b58792722c3d7d0be8d9b30c3a5df7f2c989637
Gonza Giampietri <giampietri.gonzalo@gmail.com>
2025-06-01 00:54:41 -0300

    Add initial project

---
+++ Cities/CitiesTests/CitiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesTests.swift
+//  CitiesTests
+//
+//  Created by Gonza Giampietri on 01/06/2025.
+//
+
+import Testing
+@testable import Cities
+
+struct CitiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
