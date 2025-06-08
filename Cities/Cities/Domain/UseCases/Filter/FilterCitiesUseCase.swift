0742af9302c3d54f9b712bdad303603bfb5ed75d
Gonza Giampietri <giampietri.gonzalo@gmail.com>
2025-06-08 14:56:08 -0300

    Improve search use case

--- Cities/Cities/Domain/UseCases/Filter/FilterCitiesUseCase.swift
+++ Cities/Cities/Domain/UseCases/Filter/FilterCitiesUseCase.swift
@@ -6,7 +6,15 @@
 //
 
 final class FilterCitiesUseCase: FilterCitiesUseCaseProtocol {
+    private var results: [String: [CityLocation]] = [:]
+    
     func execute(cities: [CityLocation], filterBy text: String) -> [CityLocation] {
-        cities.filter({ $0.name.lowercased().hasPrefix(text.lowercased()) })
+        guard let storedResults = results[text] else {
+            let filteredCities = cities.filter({ $0.name.lowercased().hasPrefix(text.lowercased()) })
+            results[text] = filteredCities
+            return filteredCities
+        }
+        
+        return storedResults
     }
 }
